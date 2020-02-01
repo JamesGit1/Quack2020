ca43ee21866524f813e786261dc5bc75d568d0cb
DanielTheoren <55436588+DanielTheoren@users.noreply.github.com>
2020-02-01 23:27:56 +0000

    Add files via upload

---
+++ MapTesterEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Maptester))]
+public class MapTesterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        var script = (Maptester)target;
+
+        if(GUILayout.Button("Generate Island"))
+        {
+            if (Application.isPlaying)
+            {
+                script.MakeMap();
+            }
+        }
+    }
+}
