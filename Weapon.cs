97183dd49a3fb84247ae3c7eb20af74ed4fe2f90
JamesGit1 <43144127+JamesGit1@users.noreply.github.com>
2020-02-01 22:46:44 +0000

    Add files via upload

---
+++ Weapon.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public float offset;
+
+    public GameObject projectile;
+    //public GameObject shotEffect;
+    public Transform shotPoint;
+    public bool movement = true;
+    //public Animator camAnim;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    private void Update()
+    {
+        // Handles the weapon rotation
+        if (movement == true)
+        {
+            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+        }
+        
+
+        if (timeBtwShots <= 0)
+        {
+            movement = true;
+            if (Input.GetMouseButton(0))
+            {
+                //Instantiate(shotEffect, shotPoint.position, Quaternion.identity);
+                //camAnim.SetTrigger("shake");
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+            if (gameObject.name == "GunMissile")
+            {
+                movement = false;
+            }
+        }
+
+
+    }
+}
---
+++ WeaponType.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponType
+{
+    public int damage;
+    public int range;
+    public WeaponType(int _damage, int _range)
+    {
+        damage = _damage;
+        range = _range;
+    }
+} 
